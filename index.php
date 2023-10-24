911026badbf1f67b00ccc373732d1dba5129f691
LVR1605 <aubreyamante4122@gmail.com>
2023-10-25 01:07:30 +0800

    first commit

--- index.php
+++ index.php
@@ -6,54 +6,58 @@
     <title>Cakey</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-<div class="container mx-auto">
-    <h1 class="m-5 text-3xl">Cake D.I.Y.</h1>
-    <table class="min-w-full border-black border-2">
-        <thead class="border-2 border-black">
-            <tr>
-                <th class="px-6 py-3 bg-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                    Cake Shape
-                </th>
-                <th class="px-6 py-3 bg-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                    Cake Flavor
-                </th>
-                <th class="px-6 py-3 bg-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                    Cake Toppings
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td class="px-6 py-4 whitespace-no-wrap">
-                    <fieldset>
-            
-                        <div id="cakeShapeOptions">
-                        </div>
-                    </fieldset>
-                </td>
-                <td class="px-6 py-4 whitespace-no-wrap">
-                    <fieldset>
-                        <div id="cakeFlavorOptions">
-                        </div>
-                    </fieldset>
-                </td>
-                <td class="px-6 py-4 whitespace-no-wrap">
-                    <fieldset>
-                        <div id="cakeToppingsOptions">
-                        </div>
-                    </fieldset>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+<body class="bg-gray-100">
+<div class="container mx-auto p-4 sm:p-8">
+    <h1 class="text-3xl sm:text-4xl mb-4 sm:mb-8">Cake D.I.Y.</h1>
+    <div class="overflow-x-auto">
+        <table class="min-w-full table-auto border border-black">
+            <thead class="border border-black">
+                <tr>
+                    <th class="px-4 sm:px-6 py-3 sm:py-4 bg-gray-200 text-left text-xs sm:text-sm leading-4 sm:leading-5 font-medium text-gray-600 uppercase tracking-wider">
+                        Cake Shape
+                    </th>
+                    <th class="px-4 sm:px-6 py-3 sm:py-4 bg-gray-200 text-left text-xs sm:text-sm leading-4 sm:leading-5 font-medium text-gray-600 uppercase tracking-wider">
+                        Cake Flavor
+                    </th>
+                    <th class="px-4 sm:px-6 py-3 sm:py-4 bg-gray-200 text-left text-xs sm:text-sm leading-4 sm:leading-5 font-medium text-gray-600 uppercase tracking-wider">
+                        Cake Toppings
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="px-4 sm:px-6 py-4 whitespace-no-wrap">
+                        <fieldset>
+                            <legend class="sr-only">Cake Shape</legend>
+                            <div id="cakeShapeOptions">
+                            </div>
+                        </fieldset>
+                    </td>
+                    <td class="px-4 sm:px-6 py-4 whitespace-no-wrap">
+                        <fieldset>
+                            <legend class="sr-only">Cake Flavor</legend>
+                            <div id="cakeFlavorOptions">
+                            </div>
+                        </fieldset>
+                    </td>
+                    <td class="px-4 sm:px-6 py-4 whitespace-no-wrap">
+                        <fieldset>
+                            <legend class="sr-only">Cake Toppings</legend>
+                            <div id="cakeToppingsOptions">
+                            </div>
+                        </fieldset>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
 
 <script>
     // Arrays for cake options
     const cakeShapes = ["Heart shape", "Rectangle shape", "Square shape", "Round shape"];
     const cakeFlavors = ["Chocolate", "Vanilla", "Lemon", "Cheesecake"];
-    const cakeToppings = ["Cookies", "Spun-Sugar Flowers", "Mini Chocolate Candies", "Marshmallows",];
+    const cakeToppings = ["Cookies", "Spun-Sugar Flowers", "Mini Chocolate Candies", "Marshmallows"];
 
     // Function to generate radio buttons for options
     function generateRadioOptions(containerId, options) {
@@ -70,7 +74,7 @@
             radio.value = option;
 
             const span = document.createElement("span");
-            span.className = "ml-2";
+            span.className = "ml-2 text-xs sm:text-sm";
             span.textContent = option;
 
             label.appendChild(radio);
